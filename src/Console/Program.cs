bbd530551367670d49a943d7f69b10567663d353
Tom Anderson <tom.anderson@experian.com>
2018-01-22 17:05:12 +0000

    messing around in the console

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -9,17 +9,28 @@ namespace TestHarness
         static void Main(string[] args)
         {
             var playerFactory = new PlayerFactory();
-            var myPlayerNames = new List<string> { "Thibaut Courtois", "Phil Jones", "Mohamed Salah", "Raheem Sterling" };
             var myPlayers = new List<IPlayer>();
+
+            var myPlayerNames = new List<string> {
+                "Thibaut Courtois",
+                "Phil Jones",
+                "Mohamed Salah",
+                "Raheem Sterling",
+                "Harry Kane",
+                "Marko Arnautovic",
+                "Pascal Groß"
+            };
 
             foreach (var player in myPlayerNames)
             {
                 var firstName = player.Split(' ')[0];
                 var secondName = player.Split(' ')[1];
-                myPlayers.Add(playerFactory.CreatePlayer(firstName, secondName, true));
+                var newPlayer = playerFactory.CreatePlayer(firstName, secondName, true);
+                Console.WriteLine("Player: {0} {1}, {2}", firstName, secondName, newPlayer.PointsPerNinetyPerMillion);
+                myPlayers.Add(newPlayer);
             }
 
-            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }
